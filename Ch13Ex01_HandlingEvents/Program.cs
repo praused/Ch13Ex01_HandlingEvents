37cc3ddc16ae0aab2e49c4690a9b7a596664f476
David Prause <praused@hotmail.com>
2017-09-11 10:55:20 -0400

    Example complete

--- Ch13Ex01_HandlingEvents/Program.cs
+++ Ch13Ex01_HandlingEvents/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Ch13Ex01_HandlingEvents
 {
     class Program
     {
+        static int counter = 0;
+
+        static string displayString = "This string will appear one letter at a time. ";
+
         static void Main(string[] args)
         {
+            Timer myTimer = new Timer(100);
+            myTimer.Elapsed += new ElapsedEventHandler(WriteChar);
+            ////or use method group syntax (delegate type will be inferred)..
+            //myTimer.Elapsed += WriteChar;
+            myTimer.Start();
+            Console.ReadKey();
+        }
+
+        static void WriteChar(object sender, ElapsedEventArgs e)
+        {
+            Console.Write(displayString[counter++ % displayString.Length]);
         }
     }
 }
